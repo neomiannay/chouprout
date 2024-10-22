346a0685afb63e849fbbb8250347627985659095
neo_mny <miannayneo@gmail.com>
2024-10-22 15:17:07 +0200

    Feat config ⚙️

--- src/js/settings.js
+++ src/js/settings.js
@@ -2,18 +2,18 @@ export const radius = 100;
 export const holdBarHeight = 25;
 export const hitZonePosition = window.innerWidth / 2;
 export const precision = 15;
-export const startSpeed = 3;
-export const hitRange = [hitZonePosition - precision, hitZonePosition + precision]
-export const numOfTargets = 200;
-export const arrowTypes = ['left', 'right', 'up', 'down']
-export const timelineY = window.innerHeight - 100
-export const iconScale = 2
+export const startSpeed = 5;
+export const hitRange = [hitZonePosition - precision, hitZonePosition + precision];
+export const numOfTargets = 10;
+export const arrowTypes = ['left', 'right', 'up', 'down'];
+export const timelineY = window.innerHeight - 100;
+export const iconScale = 2;
 export const smallFarts = [
     { name: 'PetitPet_1', src: './assets/audios/PetitPet_1.mp3' },
     { name: 'PetitPet_2', src: './assets/audios/PetitPet_2.mp3' },
     { name: 'PetitPet_3', src: './assets/audios/PetitPet_3.mp3' },
-]
+];
 export const longFarts = [
     { name: 'LongPet_1', src: './assets/audios/LongPet_1.mp3' },
     { name: 'LongPet_2', src: './assets/audios/LongPet_2.mp3' },
-]
+];
